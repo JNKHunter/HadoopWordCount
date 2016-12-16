b72af63af78b8010d7efaa191279f875b372c1eb
John Hunter <john@push-it-forward.com>
2016-12-15 18:10:03 -0800

    added pseudo clustered example

--- src/main/java/tech/eats/art/WordCount.java
+++ src/main/java/tech/eats/art/WordCount.java
@@ -9,8 +9,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 
@@ -21,7 +24,7 @@ public class WordCount {
 
 
 
-    public static void main(String[] args) throws Exception {
+/*    public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
@@ -33,5 +36,36 @@ public class WordCount {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+        String[] remainingArgs = optionParser.getRemainingArgs();
+        if (!(remainingArgs.length != 2 || remainingArgs.length != 4)) {
+            System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(tech.eats.art.cluster.TokenizerMapper.class);
+        job.setCombinerClass(tech.eats.art.cluster.IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(tech.eats.art.cluster.IntSumReducer.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        List<String> otherArgs = new ArrayList<String>();
+        for (int i=0; i < remainingArgs.length; ++i) {
+            if ("-skip".equals(remainingArgs[i])) {
+                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+                job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+            } else {
+                otherArgs.add(remainingArgs[i]);
+            }
+        }
+        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
